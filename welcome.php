af5de74388622ef034db754c17ac0c44cf307e75
Rohan Chaudhari <31539742+Rohanchaudhari98@users.noreply.github.com>
2018-05-28 17:38:27 +0530

    Add files via upload

---
+++ welcome.php
@@ -0,0 +1,24 @@
+<?php
+// Initialize the session
+session_start();
+ 
+// If session variable is not set it will redirect to login page
+if(!isset($_SESSION['username']) || empty($_SESSION['username'])){
+  header("location: login.php");
+  exit;
+}
+?>
+ 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Welcome</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        body{ font: 14px sans-serif; text-align: center; }
+    </style>
+</head>
+<body>
+    <h1><a href="top11lists.php"><button style=  " margin: 100px 50px 25px 80px;">Top 11 lists</button></a>
+</html>
